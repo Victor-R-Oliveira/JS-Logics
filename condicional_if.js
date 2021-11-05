13f971627fd587f1b8637d3a75f1235189ab9ac1
Victor-R-Oliveira <victorrodriguesoliveira0@gmail.com>
2021-11-04 22:42:46 -0300

    commit

--- condicional_if.js
+++ condicional_if.js
@@ -1,42 +1,40 @@
-// Verificador de idade
-
-var idade = parseInt(prompt('Digite sua idade: '))
+var idade = parseInt(prompt("Digite a sua idade: "))
+//IDADE
 var idade = 18
 var resposta = "";
-
 if (idade >= 18) {
-    resposta = 'Olá, sua idade te permite ver este site!'
+    resposta = 'Ola sua idade, deixa vc ver o conteudo desse site!';
 } else {
-    resposta = 'Sua idade não permite você ver este site!'
-    // window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" // Redirecionamento de página
+    resposta = 'Sua idade não permite vc ver o conteudo desse site!';
+    //window.location = "https://google.com"; //redicionamento da pagina
 }
+
 alert(resposta)
 
 
-// Hora
+//HORA
 
-var senha = parseInt(prompt('Digite a hora: '))
+var hora = parseInt(prompt("Digite a hora: "))
 var respostaHora;
 
-if (hora < 12) { // -999999 a 11
-    respostaHora = "Bom dia!"
-} else if (hora < 18) { // 12 a 18
-    respostaHora = "Boa tarde"
+if (hora < 12) { //-99999999 a 11
+    respostaHora = "Bom dia!";
+} else if (hora <= 18) { //12 a 18
+    respostaHora = "Boa tarde!";
 } else { // > 18
-    respostaHora = "Boa noite!"
+    respostaHora = "Boa noite!";
 }
+
 alert(respostaHora)
 
 
+//SENHA
 
-// Senha
-
-var senha = parseInt(prompt('Digite a senha: ')) // Inteiro
-var respostaSenha
-
-if (senha === 123456) { // senha: 123456 (int) === "123456" (string)
-    respostaSenha = "Bem-vindo ao sistema!"
-} else {
-    respostaSenha = "Senha incorreta!"
+var senha = prompt("Digite a senha: ") //string
+var respostaSenha;
+if (senha == 123456) { // senha: 123456 (int) === 123456 (int)
+    respostaSenha = "Senha correta! Bem vindo ao sistema!";
+}  else { 
+    respostaSenha = "Senha incorreta! Tente novamente!";
 }
 alert(respostaSenha)
---
+++ pesoIdeal.js
@@ -0,0 +1,30 @@
+// Calculadora de peso
+// Calcula o peso ideal de uma pessoa obedecendo fatores de peso pré-determinados
+
+
+btnCalcular.addEventListener('click', function () { // Clicar no botão "Calcular" irá mostrar o peso ideal do usuário
+    formNome = nome.value
+    formAltura = altura.value
+    sexoMasculino
+
+    // Condições
+    if(sexoMasculino.checked){
+        fatorPeso = 22
+    } else {
+        fatorPeso = 21
+    }
+
+    pesoFinal = fatorPeso * Math.pow(formAltura, 2).toFixed(2)
+
+    // alert(pesoFinal)
+
+    mostrarPeso = document.getElementById('pesoIdeal')
+    mostrarPeso.innerHTML = `
+        <p>Olá, ${formNome}, seu peso ideal é ${pesoFinal}.</p>`
+})
+
+var btn = document.querySelector("#btnLimpar")
+btn.addEventListener('click', function(){
+    alert("A página foi recarregada com sucesso!")
+    location.reload()
+})
